b5d7c2b4bbe2b80094c7b5423a4001c4a44eb07a
danielmmy <danielmmy10@gmail.com>
2017-03-09 15:50:41 +0000

    Added L2 cache refill to monitor

--- serial_read_test.c
+++ serial_read_test.c
@@ -12,8 +12,9 @@ enum events{
 	INSTRUCTIONS,
 	CYCLES,
 	L1_DCACHE_ACCESS,
-	L1_DCACHE_MISSES,
+	L1_DCACHE_REFILL,
 	L2_CACHE_ACCESS,
+	L2_CACHE_REFILL,
 	EVENTS_COUNT
 }EVENTS;
 
@@ -67,7 +68,7 @@ int main(int argc, char **argv){
 	int fd[EVENTS_COUNT];
 	fd[INSTRUCTIONS]=perf_event_open(&attr, 0, -1, -1, 0);
         if (fd[INSTRUCTIONS] == -1){
-                printf("cannot open perf_counter for instructions\n");
+                printf("cannot open perf_counter for INSTRUCTIONS\n");
                 exit(0);
         }
 
@@ -75,7 +76,7 @@ int main(int argc, char **argv){
 	attr.config = PERF_COUNT_HW_CPU_CYCLES;
 	fd[CYCLES]=perf_event_open(&attr, 0, -1, -1, 0);
         if (fd[CYCLES] == -1){
-                printf("cannot open perf_counter for cycles\n");
+                printf("cannot open perf_counter for CYCLES\n");
                 exit(0);
         }
 
@@ -89,9 +90,9 @@ int main(int argc, char **argv){
 
         attr.type = PERF_TYPE_HW_CACHE;
         attr.config = (PERF_COUNT_HW_CACHE_L1D)|(PERF_COUNT_HW_CACHE_OP_READ << 8)|(PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
-        fd[L1_DCACHE_MISSES]=perf_event_open(&attr, 0, -1, -1, 0);
-        if (fd[L1_DCACHE_MISSES] == -1){
-                printf("cannot open perf_counter for L1_DCACHE_MISSES\n");
+        fd[L1_DCACHE_REFILL]=perf_event_open(&attr, 0, -1, -1, 0);
+        if (fd[L1_DCACHE_REFILL] == -1){
+                printf("cannot open perf_counter for L1_DCACHE_REFILL\n");
                 exit(0);
         }
 
@@ -100,6 +101,14 @@ int main(int argc, char **argv){
         fd[L2_CACHE_ACCESS]=perf_event_open(&attr, 0, -1, -1, 0);
         if (fd[L2_CACHE_ACCESS] == -1){
                 printf("cannot open perf_counter for L2_CACHE_ACCESS\n");
+                exit(0);
+        }
+	
+	attr.type = PERF_TYPE_RAW;
+	attr.config=0x17;
+        fd[L2_CACHE_REFILL]=perf_event_open(&attr, 0, -1, -1, 0);
+        if (fd[L2_CACHE_REFILL] == -1){
+                printf("cannot open perf_counter for L2_CACHE_REFILL\n");
                 exit(0);
         }	
 
@@ -135,10 +144,12 @@ int main(int argc, char **argv){
         printf("Cycles = %llu %s.\n",perf_count(counts),counts[1]==counts[2]?"real":"scaled");
 	read(fd[L1_DCACHE_ACCESS], counts, sizeof(counts));
         printf("L1 data cache access = %llu %s.\n",perf_count(counts),counts[1]==counts[2]?"real":"scaled");
-        read(fd[L1_DCACHE_MISSES], counts, sizeof(counts));
-        printf("L1 data cache misses = %llu %s.\n",perf_count(counts),counts[1]==counts[2]?"real":"scaled");
+        read(fd[L1_DCACHE_REFILL], counts, sizeof(counts));
+        printf("L1 data cache refill = %llu %s.\n",perf_count(counts),counts[1]==counts[2]?"real":"scaled");
 	read(fd[L2_CACHE_ACCESS], counts, sizeof(counts));
-        printf("L2 cache ACCESS = %llu %s.\n",perf_count(counts),counts[1]==counts[2]?"real":"scaled");
+        printf("L2 cache access = %llu %s.\n",perf_count(counts),counts[1]==counts[2]?"real":"scaled");
+	read(fd[L2_CACHE_REFILL], counts, sizeof(counts));
+        printf("L2 cache refill = %llu %s.\n",perf_count(counts),counts[1]==counts[2]?"real":"scaled");
 	printf("Result %llu\n",res);
 
 
--- serial_read_test_register_offset_lsl_scaled.s
+++ serial_read_test_register_offset_lsl_scaled.s
@@ -21,11 +21,10 @@ perf_event_open:
 	.type	main, %function
 main:
 	stp	x29, x30, [sp, -304]!
+	adrp	x3, __stack_chk_guard
+	add	x3, x3, :lo12:__stack_chk_guard
 	mov	w2, 10
 	add	x29, sp, 0
-	stp	x23, x24, [sp, 48]
-	adrp	x23, __stack_chk_guard
-	add	x3, x23, :lo12:__stack_chk_guard
 	ldr	x0, [x1, 8]
 	ldr	x4, [x3]
 	str	x4, [x29, 296]
@@ -33,6 +32,7 @@ main:
 	add	x1, x29, 96
 	stp	x19, x20, [sp, 16]
 	stp	x21, x22, [sp, 32]
+	stp	x23, x24, [sp, 48]
 	stp	x25, x26, [sp, 64]
 	stp	x27, x28, [sp, 80]
 	bl	strtoull
@@ -45,11 +45,11 @@ main:
 	mov	x21, sp
 	cbz	x19, .L7
 	.p2align 2
-.L32:
+.L35:
 	str	x0, [x21, x0, lsl 3]
 	add	x0, x0, 1
 	cmp	x19, x0
-	bne	.L32
+	bne	.L35
 .L7:
 	add	x0, x29, 168
 	add	x1, x29, 136
@@ -82,9 +82,9 @@ main:
 	mov	x0, 241
 	bl	syscall
 	cmn	w0, #1
-	str	w0, [x29, 248]
+	str	w0, [x29, 272]
 	mov	x22, x0
-	beq	.L40
+	beq	.L43
 	mov	w4, -1
 	add	x1, x29, 136
 	mov	w3, w4
@@ -95,9 +95,9 @@ main:
 	str	xzr, [x29, 144]
 	bl	syscall
 	cmn	w0, #1
-	str	w0, [x29, 252]
+	str	w0, [x29, 276]
 	mov	x24, x0
-	beq	.L41
+	beq	.L44
 	mov	w4, -1
 	add	x1, x29, 136
 	mov	w3, w4
@@ -108,9 +108,9 @@ main:
 	str	xzr, [x29, 144]
 	bl	syscall
 	cmn	w0, #1
-	str	w0, [x29, 256]
-	mov	x27, x0
-	beq	.L42
+	str	w0, [x29, 280]
+	mov	x28, x0
+	beq	.L45
 	mov	w4, -1
 	mov	x0, 65536
 	mov	w3, w4
@@ -122,146 +122,124 @@ main:
 	str	w20, [x29, 136]
 	bl	syscall
 	cmn	w0, #1
-	str	w0, [x29, 260]
+	str	w0, [x29, 284]
+	mov	x27, x0
+	beq	.L46
+	mov	x0, 22
+	mov	w4, -1
+	mov	w3, w4
+	add	x1, x29, 136
+	str	x0, [x29, 144]
+	mov	w20, 4
+	mov	x5, 0
+	mov	w2, 0
+	mov	x0, 241
+	str	w20, [x29, 136]
+	bl	syscall
+	cmn	w0, #1
+	str	w0, [x29, 288]
 	mov	x26, x0
-	beq	.L43
-	mov	w0, 4
+	beq	.L47
+	mov	x0, 23
 	mov	w4, -1
 	add	x1, x29, 136
 	mov	w3, w4
-	str	w0, [x29, 136]
-	mov	x0, 22
+	str	x0, [x29, 144]
 	mov	x5, 0
 	mov	w2, 0
-	str	x0, [x29, 144]
 	mov	x0, 241
+	str	w20, [x29, 136]
 	bl	syscall
 	cmn	w0, #1
-	str	w0, [x29, 264]
+	str	w0, [x29, 292]
 	mov	x25, x0
-	beq	.L11
+	beq	.L12
 	mov	w0, w22
-	mov	w2, 0
-	mov	x1, 9219
-	mov	x20, 0
-	add	x20, x20, 1
-	add	x28, x29, 248
-	bl	ioctl
-	cmp	x20, 5
-	beq	.L44
+	mov	x23, 0
+	add	x20, x29, 272
+	b	.L13
+	.p2align 3
+.L14:
+	ldr	w0, [x20, x23, lsl 2]
 .L13:
-	ldr	w0, [x28, x20, lsl 2]
 	mov	w2, 0
 	mov	x1, 9219
-	add	x20, x20, 1
+	add	x23, x23, 1
 	bl	ioctl
-	cmp	x20, 5
-	bne	.L13
-.L44:
+	cmp	x23, 6
+	bne	.L14
 	mov	w0, w22
-	mov	w2, 0
-	mov	x1, 9216
-	mov	x20, 0
-	add	x20, x20, 1
-	add	x28, x29, 248
-	bl	ioctl
-	cmp	x20, 5
-	beq	.L15
-	.p2align 2
-.L45:
-	ldr	w0, [x28, x20, lsl 2]
-	mov	w2, 0
-	mov	x1, 9216
-	add	x20, x20, 1
-	bl	ioctl
-	cmp	x20, 5
-	bne	.L45
+	mov	x23, 0
+	add	x20, x29, 272
+	b	.L15
+	.p2align 3
+.L48:
+	ldr	w0, [x20, x23, lsl 2]
 .L15:
+	mov	w2, 0
+	mov	x1, 9216
+	add	x23, x23, 1
+	bl	ioctl
+	cmp	x23, 6
+	bne	.L48
 	add	x0, x29, 104
 	mov	x1, 0
 	mov	x20, 0
 	bl	gettimeofday
-	cbz	x19, .L16
+	cbz	x19, .L17
 	mov	x0, 0
 	.p2align 2
-.L17:
+.L18:
 	ldr	x1, [x21, x0, lsl 3]
 	add	x0, x0, 1
 	cmp	x19, x0
-	bne	.L17
-.L16:
+	bne	.L18
+.L17:
 	mov	w0, w22
-	mov	w2, 0
-	mov	x1, 9217
 	mov	x19, 0
-	add	x19, x19, 1
-	add	x21, x29, 248
-	bl	ioctl
-	cmp	x19, 5
-	beq	.L18
-	.p2align 2
-.L46:
+	add	x21, x29, 272
+	b	.L20
+	.p2align 3
+.L49:
 	ldr	w0, [x21, x19, lsl 2]
+.L20:
 	mov	w2, 0
 	mov	x1, 9217
 	add	x19, x19, 1
 	bl	ioctl
-	cmp	x19, 5
-	bne	.L46
-.L18:
+	cmp	x19, 6
+	bne	.L49
 	add	x0, x29, 120
 	mov	x1, 0
 	bl	gettimeofday
+	ldp	x2, x3, [x29, 104]
 	ldp	x1, x0, [x29, 120]
+	sub	x1, x1, x2
 	mov	x2, 16960
-	ldr	x3, [x29, 104]
 	movk	x2, 0xf, lsl 16
-	sub	x1, x1, x3
-	ldr	x3, [x29, 112]
 	sub	x0, x0, x3
 	madd	x2, x1, x2, x0
-	adrp	x1, .LC7
-	add	x1, x1, :lo12:.LC7
+	adrp	x1, .LC8
+	add	x1, x1, :lo12:.LC8
 	mov	w0, 1
 	bl	__printf_chk
-	adrp	x0, .LC8
-	add	x0, x0, :lo12:.LC8
+	adrp	x0, .LC9
+	add	x0, x0, :lo12:.LC9
 	bl	puts
-	add	x1, x29, 272
+	add	x1, x29, 248
 	mov	w0, w22
 	mov	x2, 24
 	bl	read
-	ldp	x0, x2, [x29, 272]
-	ucvtf	s2, x2
-	ldr	x1, [x29, 288]
+	ldp	x1, x0, [x29, 256]
+	ucvtf	s1, x0
+	ucvtf	s2, x1
+	cmp	x1, x0
+	ldr	x0, [x29, 248]
 	ucvtf	s0, x0
-	ucvtf	s1, x1
-	cmp	x2, x1
 	fmul	s0, s0, s2
 	fdiv	s0, s0, s1
 	fcvtzu	x2, s0
-	beq	.L47
-	adrp	x3, .LC1
-	add	x3, x3, :lo12:.LC1
-.L20:
-	adrp	x1, .LC9
-	mov	w0, 1
-	add	x1, x1, :lo12:.LC9
-	bl	__printf_chk
-	add	x1, x29, 272
-	mov	w0, w24
-	mov	x2, 24
-	bl	read
-	ldp	x0, x2, [x29, 272]
-	ucvtf	s2, x2
-	ldr	x1, [x29, 288]
-	ucvtf	s0, x0
-	ucvtf	s1, x1
-	cmp	x2, x1
-	fmul	s0, s0, s2
-	fdiv	s0, s0, s1
-	fcvtzu	x2, s0
-	beq	.L48
+	beq	.L50
 	adrp	x3, .LC1
 	add	x3, x3, :lo12:.LC1
 .L21:
@@ -269,55 +247,13 @@ main:
 	mov	w0, 1
 	add	x1, x1, :lo12:.LC10
 	bl	__printf_chk
-	add	x1, x29, 272
-	mov	w0, w27
+	add	x1, x29, 248
+	mov	w0, w24
 	mov	x2, 24
 	bl	read
-	ldp	x0, x2, [x29, 272]
+	ldp	x0, x2, [x29, 248]
 	ucvtf	s2, x2
-	ldr	x1, [x29, 288]
-	ucvtf	s0, x0
-	ucvtf	s1, x1
-	cmp	x2, x1
-	fmul	s0, s0, s2
-	fdiv	s0, s0, s1
-	fcvtzu	x2, s0
-	beq	.L49
-	adrp	x3, .LC1
-	add	x3, x3, :lo12:.LC1
-.L22:
-	adrp	x1, .LC11
-	mov	w0, 1
-	add	x1, x1, :lo12:.LC11
-	bl	__printf_chk
-	add	x1, x29, 272
-	mov	w0, w26
-	mov	x2, 24
-	bl	read
-	ldp	x0, x2, [x29, 272]
-	ucvtf	s2, x2
-	ldr	x1, [x29, 288]
-	ucvtf	s0, x0
-	ucvtf	s1, x1
-	cmp	x2, x1
-	fmul	s0, s0, s2
-	fdiv	s0, s0, s1
-	fcvtzu	x2, s0
-	beq	.L50
-	adrp	x3, .LC1
-	add	x3, x3, :lo12:.LC1
-.L23:
-	adrp	x1, .LC12
-	mov	w0, 1
-	add	x1, x1, :lo12:.LC12
-	bl	__printf_chk
-	add	x1, x29, 272
-	mov	w0, w25
-	mov	x2, 24
-	bl	read
-	ldp	x0, x2, [x29, 272]
-	ucvtf	s2, x2
-	ldr	x1, [x29, 288]
+	ldr	x1, [x29, 264]
 	ucvtf	s0, x0
 	ucvtf	s1, x1
 	cmp	x2, x1
@@ -327,22 +263,107 @@ main:
 	beq	.L51
 	adrp	x3, .LC1
 	add	x3, x3, :lo12:.LC1
+.L22:
+	adrp	x1, .LC11
+	mov	w0, 1
+	add	x1, x1, :lo12:.LC11
+	bl	__printf_chk
+	add	x1, x29, 248
+	mov	w0, w28
+	mov	x2, 24
+	bl	read
+	ldp	x0, x2, [x29, 248]
+	ucvtf	s2, x2
+	ldr	x1, [x29, 264]
+	ucvtf	s0, x0
+	ucvtf	s1, x1
+	cmp	x2, x1
+	fmul	s0, s0, s2
+	fdiv	s0, s0, s1
+	fcvtzu	x2, s0
+	beq	.L52
+	adrp	x3, .LC1
+	add	x3, x3, :lo12:.LC1
+.L23:
+	adrp	x1, .LC12
+	mov	w0, 1
+	add	x1, x1, :lo12:.LC12
+	bl	__printf_chk
+	add	x1, x29, 248
+	mov	w0, w27
+	mov	x2, 24
+	bl	read
+	ldp	x0, x2, [x29, 248]
+	ucvtf	s2, x2
+	ldr	x1, [x29, 264]
+	ucvtf	s0, x0
+	ucvtf	s1, x1
+	cmp	x2, x1
+	fmul	s0, s0, s2
+	fdiv	s0, s0, s1
+	fcvtzu	x2, s0
+	beq	.L53
+	adrp	x3, .LC1
+	add	x3, x3, :lo12:.LC1
 .L24:
 	adrp	x1, .LC13
 	mov	w0, 1
 	add	x1, x1, :lo12:.LC13
-	add	x23, x23, :lo12:__stack_chk_guard
 	bl	__printf_chk
+	add	x1, x29, 248
+	mov	w0, w26
+	mov	x2, 24
+	bl	read
+	ldp	x0, x2, [x29, 248]
+	ucvtf	s2, x2
+	ldr	x1, [x29, 264]
+	ucvtf	s0, x0
+	ucvtf	s1, x1
+	cmp	x2, x1
+	fmul	s0, s0, s2
+	fdiv	s0, s0, s1
+	fcvtzu	x2, s0
+	beq	.L54
+	adrp	x3, .LC1
+	add	x3, x3, :lo12:.LC1
+.L25:
 	adrp	x1, .LC14
-	mov	x2, x20
+	mov	w0, 1
 	add	x1, x1, :lo12:.LC14
+	bl	__printf_chk
+	add	x1, x29, 248
+	mov	w0, w25
+	mov	x2, 24
+	bl	read
+	ldp	x0, x2, [x29, 248]
+	ucvtf	s2, x2
+	ldr	x1, [x29, 264]
+	ucvtf	s0, x0
+	ucvtf	s1, x1
+	cmp	x2, x1
+	fmul	s0, s0, s2
+	fdiv	s0, s0, s1
+	fcvtzu	x2, s0
+	beq	.L55
+	adrp	x3, .LC1
+	add	x3, x3, :lo12:.LC1
+.L26:
+	adrp	x1, .LC15
+	mov	w0, 1
+	add	x1, x1, :lo12:.LC15
+	bl	__printf_chk
+	adrp	x1, .LC16
+	mov	x2, x20
+	add	x1, x1, :lo12:.LC16
 	mov	w0, 1
 	bl	__printf_chk
+	adrp	x0, __stack_chk_guard
+	add	x23, x0, :lo12:__stack_chk_guard
 	ldr	x0, [x29, 296]
 	ldr	x1, [x23]
 	eor	x1, x0, x1
 	mov	w0, 0
-	cbnz	x1, .L52
+	cbnz	x1, .L56
 	add	sp, x29, 0
 	ldp	x19, x20, [sp, 16]
 	ldp	x21, x22, [sp, 32]
@@ -351,55 +372,65 @@ main:
 	ldp	x27, x28, [sp, 80]
 	ldp	x29, x30, [sp], 304
 	ret
-.L47:
-	adrp	x3, .LC0
-	add	x3, x3, :lo12:.LC0
-	b	.L20
 .L50:
 	adrp	x3, .LC0
 	add	x3, x3, :lo12:.LC0
-	b	.L23
-.L49:
-	adrp	x3, .LC0
-	add	x3, x3, :lo12:.LC0
-	b	.L22
-.L48:
-	adrp	x3, .LC0
-	add	x3, x3, :lo12:.LC0
 	b	.L21
-.L51:
+.L54:
+	adrp	x3, .LC0
+	add	x3, x3, :lo12:.LC0
+	b	.L25
+.L53:
 	adrp	x3, .LC0
 	add	x3, x3, :lo12:.LC0
 	b	.L24
 .L52:
-	bl	__stack_chk_fail
-.L11:
-	adrp	x0, .LC6
-	add	x0, x0, :lo12:.LC6
-	bl	puts
-	mov	w0, 0
-	bl	exit
-.L43:
-	adrp	x0, .LC5
-	add	x0, x0, :lo12:.LC5
-	bl	puts
-	mov	w0, 0
-	bl	exit
-.L42:
+	adrp	x3, .LC0
+	add	x3, x3, :lo12:.LC0
+	b	.L23
+.L51:
+	adrp	x3, .LC0
+	add	x3, x3, :lo12:.LC0
+	b	.L22
+.L55:
+	adrp	x3, .LC0
+	add	x3, x3, :lo12:.LC0
+	b	.L26
+.L45:
 	adrp	x0, .LC4
 	add	x0, x0, :lo12:.LC4
 	bl	puts
 	mov	w0, 0
 	bl	exit
-.L41:
+.L56:
+	bl	__stack_chk_fail
+.L44:
 	adrp	x0, .LC3
 	add	x0, x0, :lo12:.LC3
 	bl	puts
 	mov	w0, 0
 	bl	exit
-.L40:
+.L43:
 	adrp	x0, .LC2
 	add	x0, x0, :lo12:.LC2
+	bl	puts
+	mov	w0, 0
+	bl	exit
+.L12:
+	adrp	x0, .LC7
+	add	x0, x0, :lo12:.LC7
+	bl	puts
+	mov	w0, 0
+	bl	exit
+.L47:
+	adrp	x0, .LC6
+	add	x0, x0, :lo12:.LC6
+	bl	puts
+	mov	w0, 0
+	bl	exit
+.L46:
+	adrp	x0, .LC5
+	add	x0, x0, :lo12:.LC5
 	bl	puts
 	mov	w0, 0
 	bl	exit
@@ -414,41 +445,47 @@ main:
 	.string	"scaled"
 	.zero	1
 .LC2:
-	.string	"cannot open perf_counter for instructions"
+	.string	"cannot open perf_counter for INSTRUCTIONS"
 	.zero	6
 .LC3:
-	.string	"cannot open perf_counter for cycles"
+	.string	"cannot open perf_counter for CYCLES"
 	.zero	4
 .LC4:
 	.string	"cannot open perf_counter for L1_DCACHE_ACCESS"
 	.zero	2
 .LC5:
-	.string	"cannot open perf_counter for L1_DCACHE_MISSES"
+	.string	"cannot open perf_counter for L1_DCACHE_REFILL"
 	.zero	2
 .LC6:
 	.string	"cannot open perf_counter for L2_CACHE_ACCESS"
 	.zero	3
 .LC7:
+	.string	"cannot open perf_counter for L2_CACHE_REFILL"
+	.zero	3
+.LC8:
 	.string	"Wall clock time[gettimeofday(2)] = %llu us\n"
 	.zero	4
-.LC8:
-	.string	"PMU statistics:"
 .LC9:
+	.string	"PMU statistics:"
+.LC10:
 	.string	"Instructions = %llu %s.\n"
 	.zero	7
-.LC10:
+.LC11:
 	.string	"Cycles = %llu %s.\n"
 	.zero	5
-.LC11:
+.LC12:
 	.string	"L1 data cache access = %llu %s.\n"
 	.zero	7
-.LC12:
-	.string	"L1 data cache misses = %llu %s.\n"
-	.zero	7
 .LC13:
-	.string	"L2 cache ACCESS = %llu %s.\n"
-	.zero	4
+	.string	"L1 data cache refill = %llu %s.\n"
+	.zero	7
 .LC14:
+	.string	"L2 cache access = %llu %s.\n"
+	.zero	4
+.LC15:
+	.string	"L2 cache refill = %llu %s.\n"
+	.zero	4
+.LC16:
 	.string	"Result %llu\n"
-	.ident	"GCC: (Ubuntu/Linaro 5.4.1-2ubuntu1~16.04) 5.4.1 20160904"
+	.ident	"GCC: (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
 	.section	.note.GNU-stack,"",@progbits
