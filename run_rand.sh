a0d8cc96c05f9d5dd8e5bbcdcb5d7f19d1db1fc2
danielmmy <danielmmy10@gmail.com>
2017-03-10 18:26:40 +0000

    Added random write experiments to execution

--- run_rand.sh
+++ run_rand.sh
@@ -10,7 +10,7 @@ fi
 
 mkdir -p ./results/rand
 
-rand_read_experiments=("rand_read_test_simple" "rand_read_test_simple_11xadd" "rand_read_test_simple_11xbranch" "rand_read_test_simple_11xshift" "rand_read_test_register_offset_lsl_scaled" "rand_read_test_register_offset_lsl_scaled_11xbranch" "rand_read_test_register_offset_multiply" "rand_read_test_register_offset_multiply_11xbranch" "rand_read_test_register_offset_multiply_11xmultipy" "rand_read_test_register_offset_shift" "rand_read_test_register_offset_shift_11xbranch" "rand_read_test_register_offset_shift_11xshift")
+rand_read_experiments=("rand_read_test_simple" "rand_write_test_simple" "rand_read_test_simple_11xadd" "rand_read_test_simple_11xbranch" "rand_read_test_simple_11xshift" "rand_read_test_register_offset_lsl_scaled" "rand_write_test_register_offset_lsl_scaled" "rand_read_test_register_offset_lsl_scaled_11xbranch" "rand_read_test_register_offset_multiply" "rand_write_test_register_offset_multiply" "rand_read_test_register_offset_multiply_11xbranch" "rand_read_test_register_offset_multiply_11xmultipy" "rand_read_test_register_offset_shift" "rand_write_test_register_offset_shift" "rand_read_test_register_offset_shift_11xbranch" "rand_read_test_register_offset_shift_11xshift")
 
 for (( i=0; i<$executions ;i++ ))
 do
