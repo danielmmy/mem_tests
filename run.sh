cd4b2d45732d0cbcc292e57673a8c7d1cf12bfa6
danielmmy <danielmmy10@gmail.com>
2017-03-11 11:06:31 +0000

    Master script to run all experiments

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sizes=("1000" "10000" "100000" "1000000" "10000000" "100000000")
+executions=1
+if [ "$#" -eq 1 ];then
+	executions="$1"
+fi
+
+for (( i=0; i<$executions ;i++ ))
+do
+	for j in "${sizes[@]}"
+	do
+		./run_rand.sh $j
+		./run_serial.sh $j
+	done
+done
+
