f47238b41d635aca7796aa87bf04d2b367f3d98a
danielmmy <danielmmy10@gmail.com>
2017-03-11 11:07:13 +0000

    Results output are now subdivided into vector size

--- run_rand.sh
+++ run_rand.sh
@@ -8,7 +8,9 @@ if [ "$#" -eq 2 ];then
 	executions="$2"
 fi
 
-mkdir -p ./results/rand
+output=./results/rand$size
+
+mkdir -p $output
 
 rand_read_experiments=("rand_read_test_simple" "rand_write_test_simple" "rand_read_test_simple_11xadd" "rand_read_test_simple_11xbranch" "rand_read_test_simple_11xshift" "rand_read_test_register_offset_lsl_scaled" "rand_write_test_register_offset_lsl_scaled" "rand_read_test_register_offset_lsl_scaled_11xbranch" "rand_read_test_register_offset_multiply" "rand_write_test_register_offset_multiply" "rand_read_test_register_offset_multiply_11xbranch" "rand_read_test_register_offset_multiply_11xmultipy" "rand_read_test_register_offset_shift" "rand_write_test_register_offset_shift" "rand_read_test_register_offset_shift_11xbranch" "rand_read_test_register_offset_shift_11xshift")
 
@@ -16,9 +18,9 @@ for (( i=0; i<$executions ;i++ ))
 do
 	for j in "${rand_read_experiments[@]}"
 	do
-		echo "experiment "$j" with vector of "$size >> ./results/rand/$j.res
-		./bin/$j ./bin/experiment$size $size >> ./results/rand/$j.res
-		echo >> ./results/rand/$j.res
+		echo "experiment "$j" with vector of "$size >> $output/$j.res
+		./bin/$j ./bin/experiment$size $size >> $output/$j.res
+		echo >> $output/$j.res
 	done
 done
 
--- run_serial.sh
+++ run_serial.sh
@@ -8,7 +8,9 @@ if [ "$#" -eq 2 ];then
 	executions="$2"
 fi
 
-mkdir -p ./results/serial
+output=./results/serial$size
+
+mkdir -p $output
 
 serial_experiments=("serial_read_test_2x_post_index" "serial_read_test_2x_post_index_11xbranch" "serial_read_test_2x_post_index_11xsub" "serial_read_test_2x_pre_index" "serial_read_test_2x_pre_index_11xbranch" "serial_read_test_2x_pre_index_11xsub" "serial_read_test_2x_simple" "serial_read_test_2x_simple_11xadd" "serial_read_test_2x_simple_11xbranch" "serial_read_test_2x_simple_11xcmp" "serial_read_test_post_index" "serial_read_test_post_index_11xbranch" "serial_read_test_post_index_11xsub" "serial_read_test_pre_index" "serial_read_test_pre_index_11xbranch" "serial_read_test_pre_index_11xsub" "serial_read_test_register_offset_lsl_scaled" "serial_read_test_register_offset_shift" "serial_read_test_register_offset_shift_11xadd" "serial_read_test_register_offset_shift_11xbranch" "serial_read_test_register_offset_shift_11xcmp" "serial_read_test_register_offset_shift_11xshift" "serial_read_test_simple" "serial_read_test_simple_11xadd" "serial_read_test_simple_11xbranch" "serial_read_test_simple_11xcmp" "serial_write_test_2x_post_index" "serial_write_test_2x_post_index_11xbranch" "serial_write_test_2x_post_index_11xsub" "serial_write_test_2x_pre_index" "serial_write_test_2x_pre_index_11xbranch" "serial_write_test_2x_pre_index_11xsub" "serial_write_test_2x_simple" "serial_write_test_2x_simple_11xadd" "serial_write_test_2x_simple_11xbranch" "serial_write_test_2x_simple_11xcmp" "serial_write_test_post_index" "serial_write_test_post_index_11xbranch" "serial_write_test_post_index_11xsub" "serial_write_test_pre_index" "serial_write_test_pre_index_11xbranch" "serial_write_test_pre_index_11xsub" "serial_write_test_register_offset_lsl_scaled" "serial_write_test_register_offset_shift" "serial_write_test_register_offset_shift_11xadd" "serial_write_test_register_offset_shift_11xbranch" "serial_write_test_register_offset_shift_11xcmp" "serial_write_test_register_offset_shift_11xshift" "serial_write_test_simple" "serial_write_test_simple_11xadd" "serial_write_test_simple_11xbranch" "serial_write_test_simple_11xcmp" )
 
@@ -17,8 +19,8 @@ for (( i=0; i<$executions ;i++ ))
 do
         for j in "${serial_experiments[@]}"
         do
-                echo "experiment "$j" with vector of "$size >> ./results/serial/$j.res
-                ./bin/$j $size >> ./results/serial/$j.res
-                echo >> ./results/serial/$j.res
+                echo "experiment "$j" with vector of "$size >> $output/$j.res
+                ./bin/$j $size >> $output/$j.res
+                echo >> $output/$j.res
         done
 done
